ee891f9958081e2d53ca090c75ca828b293b8df6
CharlesSedden3 <CSedden@qa.com>
2021-08-19 11:27:50 +0100

    again

--- pizzeria-schema.sql
+++ pizzeria-schema.sql
@@ -10,6 +10,12 @@ CREATE TABLE Pizza(
     PRIMARY KEY (pizza_id)
 );
 
+CREATE TABLE Customer(
+	customer_id INT AUTO_INCREMENT,
+	payment_id INT,
+	PRIMARY KEY (customer_id)
+);
+
 CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT,
     date_booking DATETIME NOT NULL,
@@ -18,14 +24,4 @@ CREATE TABLE Orders(
     PRIMARY KEY (order_id),
     FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
-);
-
-CREATE TABLE Customer(
-	customer_id INT AUTO_INCREMENT,
-	pizza_id INT,
-	order_id INT,
-	payment_id INT,
-	PRIMARY KEY (customer_id),
-	FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id)
 );
