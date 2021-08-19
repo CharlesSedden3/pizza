f00fc495ca8b6b42a6a21c369cac242e24cdb332
CharlesSedden3 <CSedden@qa.com>
2021-08-19 10:00:35 +0100

    first commit

---
+++ code/pizzeria-schema.sql
@@ -0,0 +1,36 @@
+--?.Creating and using database
+CREATE DATABASE Pizzeria;
+USE Pizzeria;
+
+
+--?.Creating tables
+CREATE TABLE Pizza(
+    pizza_id INT AUTO_INCREMENT,
+    pizza_type VARCHAR(40) NOT NULL,
+    price DECIMAL NOT NULL,
+    vegetarian BOOLEAN NOT NULL,
+    PRIMARY KEY (pizza_id)
+);
+
+
+CREATE TABLE Customer(
+	customer_id INT AUTO_INCREMENT,
+	pizza_id INT,
+	order_id INT,
+	payment_id INT,
+	PRIMARY KEY (customer_id),
+	FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+
+CREATE TABLE Orders(
+    order_id INT AUTO_INCREMENT,
+    date_booking DATETIME NOT NULL,
+    pizza_id INT,
+    customer_id INT,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
